fa97a8964c46ffe5fb65dff1f4097877b3af1e8e
joonsik <ryan0@kakao.com>
2022-06-09 22:53:15 +0900

    19. 코틀린의 이모저모 ## 1. Type Alias와 as import ## 2. 구조분해와 componentN 함수 ## 3. Jump와 Label ## 4. Takelf와 TakeUnless

---
+++ src/main/kotlin/chapter05/lec19/a/PrintHelloWorld.kt
@@ -0,0 +1,5 @@
+package chapter05.lec19.a
+
+fun printHelloWorld(){
+    println("Hello World in package a")
+}
---
+++ src/main/kotlin/chapter05/lec19/b/PrintHelloWorld.kt
@@ -0,0 +1,5 @@
+package chapter05.lec19.b
+
+fun printHelloWorld(){
+    println("Hello World in package b")
+}
